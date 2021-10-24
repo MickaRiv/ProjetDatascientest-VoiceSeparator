5409e77f987169cdba5bfee26fe3f45f8ef11121
MickaRiv <60695230+MickaRiv@users.noreply.github.com>
2021-10-24 20:48:54 +0200

    Update models.py

--- voicesep/models.py
+++ voicesep/models.py
@@ -1,13 +1,14 @@
 import numpy as np
-from nussl import separation,evaluation
+from nussl import separation, evaluation
 from dataviz import visualize_and_embed
+import pandas as pd
 
-def _report_sdr(approach, scores):
+def _report_sdr(alg_name, scores):
     SDR = {}
     SIR = {}
     SAR = {}
-    print(approach)
-    print(''.join(['-' for i in range(len(approach))]))
+    print(alg_name)
+    print(''.join(['-' for i in range(len(alg_name))]))
     for key in scores:
         if key not in ['combination', 'permutation']:
             SDR[key] = np.mean(scores[key]['SI-SDR'])
@@ -19,16 +20,24 @@ def _report_sdr(approach, scores):
             print()
     print()
 
-def run_viz_and_evaluate(alg,sources_list):
+def evaluate_model(alg, truth):
     alg_estimates = alg()
 
     if isinstance(alg, separation.primitive.HPSS):
         alg_estimates = alg_estimates[::-1]
 
     visualize_and_embed(alg_estimates)
-    print(sources_list)
+    print(truth)
     bss = evaluation.BSSEvalScale(
-        sources_list, alg_estimates,
-        source_labels=['acc', 'vox'])
+        truth, alg_estimates,
+        source_labels=['accompaniment', 'voice'])
     scores = bss.evaluate()
     _report_sdr(str(alg).split(' on')[0], scores)
+    return scores
+
+def evaluate_dict_models(alg_dict, truth):
+    scores = []
+    for name, alg in alg_dict.items():
+        score = evaluate_model(alg, truth)
+        scores.append(score.update({"Alg":name}))
+    return pd.DataFrame.from_records(scores).set_index("Alg")
