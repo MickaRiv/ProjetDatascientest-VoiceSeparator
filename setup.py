2b7db218919feff113af2c79171355d637b07667
MickaRiv <60695230+MickaRiv@users.noreply.github.com>
2021-10-24 19:50:11 +0200

    test find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="voice-separator",
       version="0.0.1",
-      packages=["voicesep"])
+      packages=find_packages())
