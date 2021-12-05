53d00d13be38d9b5c89ad5800dcdc81c0de40d60
MickaRiv <60695230+MickaRiv@users.noreply.github.com>
2021-12-05 13:40:02 +0100

    Update openunmix.py

--- voicesep/openunmix.py
+++ voicesep/openunmix.py
@@ -9,11 +9,11 @@ class OpenUnmixModel:
         self.model = model
         
     def __call__(self):
-        preds = separate(Tensor(self.mix.audio_data),
+        estimates = separate(Tensor(self.mix.audio_data),
                          self.mix.sample_rate,
                          model_str_or_path=self.model)
         preds = {}
         for stem in ["bass","drums","other","vocals"]:
-            preds[stem] = nussl.AudioSignal(audio_data_array=preds[stem][0].numpy(),
+            preds[stem] = nussl.AudioSignal(audio_data_array=estimates[stem][0].numpy(),
                                             sample_rate=self.mix.sample_rate)
         return [preds["bass"]+preds["drums"]+preds["other"], preds["vocals"]]
