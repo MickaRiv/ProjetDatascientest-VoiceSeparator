79d1945291a92fdb503ec8d899a30dc7adb22f98
MickaRiv <60695230+MickaRiv@users.noreply.github.com>
2021-12-05 13:07:36 +0100

    Create openunmix.py

---
+++ voicesep/openunmix.py
@@ -0,0 +1,19 @@
+from openunmix import separate
+from torch import Tensor
+import nussl
+
+class OpenUnmixModel:
+    
+    def __init__(self,mix,model):
+        self.mix = mix
+        self.model = model
+        
+    def __call__(self):
+        preds = separate(Tensor(self.mix.audio_data),
+                         self.mix.sample_rate,
+                         model_str_or_path=self.model)
+        preds = {}
+        for stem in ["bass","drums","other","vocals"]:
+            preds[stem] = nussl.AudioSignal(audio_data_array=preds[stem][0].numpy(),
+                                            sample_rate=self.mix.sample_rate)
+        return [preds["bass"]+preds["drums"]+preds["other"], preds["vocals"]]
